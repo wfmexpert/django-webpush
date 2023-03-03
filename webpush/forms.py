ab74e634691b7b1060127444d0baaab054043d8a
Kozyrev Ivan <74504402+KozyrevIvan@users.noreply.github.com>
2023-03-03 17:08:41 +0500

    Update forms.py

--- webpush/forms.py
+++ webpush/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-from django.utils.translation import gettext_lazy as _
+try:
+    from django.utils.translation import gettext as _
+except:
+    from django.utils.translation import gettext_lazy as _
 
 from .models import Group, PushInformation, SubscriptionInfo
 
